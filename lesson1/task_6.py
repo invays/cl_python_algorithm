a04057735b035e8575f5192831e07cb3064b5ed5
invays <ayan.paul1234@yandex.ru>
2020-11-03 14:08:13 +0300

    Урок 1

---
+++ lesson1/task_6.py
@@ -0,0 +1,57 @@
+
+user_inp = int(input("Порядковый номер в алфавите: "))
+"abcdefghijklmnopqrstuvwxyz"
+if user_inp == 1:
+    print("a")
+elif user_inp == 2:
+    print("b")
+elif user_inp == 3:
+    print("c")
+elif user_inp == 4:
+    print("d")
+elif user_inp == 5:
+    print("e")
+elif user_inp == 6:
+    print("f")
+elif user_inp == 7:
+    print("g")
+elif user_inp == 8:
+    print("h")
+elif user_inp == 9:
+    print("i")
+elif user_inp == 10:
+    print("j")
+elif user_inp == 11:
+    print("k")
+elif user_inp == 12:
+    print("l")
+elif user_inp == 13:
+    print("m")
+elif user_inp == 14:
+    print("n")
+elif user_inp == 15:
+    print("o")
+elif user_inp == 16:
+    print("p")
+elif user_inp == 17:
+    print("q")
+elif user_inp == 18:
+    print("r")
+elif user_inp == 19:
+    print("s")
+elif user_inp == 20:
+    print("t")
+elif user_inp == 21:
+    print("u")
+elif user_inp == 22:
+    print("v")
+elif user_inp == 23:
+    print("w")
+elif user_inp == 24:
+    print("x")
+elif user_inp == 25:
+    print("y")
+elif user_inp == 26:
+    print("z")
+else:
+    print("Нет такой буквы по счету в алфавите")
